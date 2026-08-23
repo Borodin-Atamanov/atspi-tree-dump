6fff186db5b2be126d8494b9776a1b206b67013f
Borodin-Atamanov <argentidin@gmail.com>
2026-08-23 16:29:29 -0300

    Ignore dump-* dirs, portability path, untrack system settings dumps

--- run_dump.sh
+++ run_dump.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 # Wrapper for the Meta+Ctrl+Q global shortcut. Runs the AT-SPI dump script from
 # this repository, then shows a one-second notification with the result.
-cd /home/i/Downloads/atspi_dump
+work_dir=${HOME}/Downloads/atspi_dump
+mkdir -pv ${work_dir}
+cd ${work_dir}
 output=$(uv run atspi_dump_json.py 2>&1)
 status=$?
 notify-send -t 3777 "AT-SPI dump" "$output"
--- src/atspi_tree_dump/__init__.py
+++ src/atspi_tree_dump/__init__.py
@@ -1,3 +1,3 @@
 """atspi-tree-dump package: the version source of the repository."""
 
-__version__ = "0.1.16"
+__version__ = "0.1.17"
